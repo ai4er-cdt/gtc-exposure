e5909a2fb8eb2cbd7a1e0657cb2200c96aee27ed
Matt <mja78@cam.ac.uk>
2021-03-25 17:02:23 +0000

    Shell script to download demo data/model

---
+++ settlement_segmentation/randomforest/download-demo-data.sh
@@ -0,0 +1,5 @@
+#!/bin/bash
+
+wget -O killick_val.tiff "https://drive.google.com/uc?export=download&id=1sjnERn_H-4TvRJ_ffRkIoc79tKNkEAV2"
+wget -O killick_classifier.pkl "https://drive.google.com/uc?export=download&id=1D3y9odtvpVRx-ZTCq2L8aHydPETAp2LD"
+
