10ebe282d3a15cb374d69b9073d9462f0f8ac5aa
Ira Shokar <Ira Shokar>
2021-04-01 12:49:32 +0100

    fix build path

--- binder/download_data.sh
+++ binder/download_data.sh
@@ -3,24 +3,24 @@
 ### Settlement Segmentation ###
 
 # Random Forrest Classifier
-sh ./../settlement_segmentation/randomforest/download-data.sh
+sh ./gtc-exposure/settlement_segmentation/randomforest/download-data.sh
 
 # Unclassified Learning - UCM Land Use Data
-sh ./../settlement_segmentation/liunsupervised/download-ucm-data.sh
+sh ./gtc-exposure/settlement_segmentation/liunsupervised/download-ucm-data.sh
 
 # SUnclassified Learning - Cloud Free Sentinel Data
-sh ./../settlement_segmentation/data/cloud_free/download-sentinel-data.sh
+sh ./gtc-exposure/settlement_segmentation/data/cloud_free/download-sentinel-data.sh
 
 
 ### Change Detection ###
 
 # Ratio Method
-sh ./../change_detection/ratio_method/download_data.sh
+sh ./gtc-exposure/change_detection/ratio_method/download_data.sh
 
 # XBD High Resolution
-sh ./../change_detection/xbd_hi_resolution_attempt/download-data.sh
+sh ./gtc-exposure/change_detection/xbd_hi_resolution_attempt/download-data.sh
 
 
 ### Exposure Quantification ###
 
-sh ./../exposure_quantification/download-data.sh
+sh ./gtc-exposure/exposure_quantification/download-data.sh
--- binder/download_demo_data.sh
+++ binder/download_demo_data.sh
@@ -2,12 +2,12 @@
 
 ### Settlement Segmentation ###
 # Random Forrest Classifier
-sh ./../settlement_segmentation/rrandomforest/download-demo-data.sh
+sh ./gtc-exposure/settlement_segmentation/rrandomforest/download-demo-data.sh
 
 ### Change Detection ###
 # Ratio Method
-sh ./../change_detection/ratio_method/download_data.sh
+sh ./gtc-exposure/change_detection/ratio_method/download_data.sh
 
 
 ### Exposure Quantification ###
-sh ./../exposure_quantification/download-data.sh
+sh ./gtc-exposure/exposure_quantification/download-data.sh
