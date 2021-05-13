72a9760862a6f565a1e0bd63f5098ee13309579b
Ira Shokar <ira.shokar@gmail.com>
2021-05-13 22:48:08 +0000

    alter download paths

--- change_detection/ratio_method/download_data.sh
+++ change_detection/ratio_method/download_data.sh
@@ -2,34 +2,34 @@
 
 pip install gdown
 
-pwd='../change_detection/ratio_method/download_data'
+mkdir change_detection/ratio_method/download_data/
 
 # results
-gdown -O $(pwd)/results.zip "https://drive.google.com/uc?export=download&id=18AngZi11uDFjNRYlM0kSdUPRB5Q0O8_v"
-unzip $(pwd)/results.zip
-rm $(pwd)/results.zip
+gdown -O change_detection/ratio_method/download_data/results.zip "https://drive.google.com/uc?export=download&id=18AngZi11uDFjNRYlM0kSdUPRB5Q0O8_v"
+unzip change_detection/ratio_method/download_data/results.zip
+rm change_detection/ratio_method/download_data/results.zip
 
 # records
-gdown -O $(pwd)/records.zip "https://drive.google.com/uc?export=download&id=1PoXkx9sf5Aiw6HYIetTGFTrgy_JPOWb0"
-unzip $(pwd)/records.zip
-rm $(pwd)/records.zip
+gdown -O change_detection/ratio_method/download_data/records.zip "https://drive.google.com/uc?export=download&id=1PoXkx9sf5Aiw6HYIetTGFTrgy_JPOWb0"
+unzip change_detection/ratio_method/download_data/records.zip
+rm change_detection/ratio_method/download_data/records.zip
 
 # models
-gdown -O $(pwd)/models.zip "https://drive.google.com/uc?export=download&id=18wdEMvBKjXdM8zB2kD9VRU2KO3v4ouBR"
-unzip $(pwd)/models.zip
-rm $(pwd)/models.zip
+gdown -O change_detection/ratio_method/download_data/models.zip "https://drive.google.com/uc?export=download&id=18wdEMvBKjXdM8zB2kD9VRU2KO3v4ouBR"
+unzip change_detection/ratio_method/download_data/models.zip
+rm change_detection/ratio_method/download_data/models.zip
 
 # gradings
-gdown -O $(pwd)/gradings.zip "https://drive.google.com/uc?export=download&id=14gH7lV6nGoaN4GQjzIwFVDT84J6QEMiT"
-unzip $(pwd)/gradings.zip
-rm $(pwd)/gradings.zip
+gdown -O change_detection/ratio_method/download_data/gradings.zip "https://drive.google.com/uc?export=download&id=14gH7lV6nGoaN4GQjzIwFVDT84J6QEMiT"
+unzip change_detection/ratio_method/download_data/gradings.zip
+rm change_detection/ratio_method/download_data/gradings.zip
 
 # geojsons
-gdown -O $(pwd)/geojsons.zip "https://drive.google.com/uc?export=download&id=1ZDzQKo37f51ZC2uRVjT8YNSusaNODRol"
-unzip $(pwd)/geojsons.zip
-rm $(pwd)/geojsons.zip
+gdown -O change_detection/ratio_method/download_data/geojsons.zip "https://drive.google.com/uc?export=download&id=1ZDzQKo37f51ZC2uRVjT8YNSusaNODRol"
+unzip change_detection/ratio_method/download_data/geojsons.zip
+rm change_detection/ratio_method/download_data/geojsons.zip
 
 # coastlines
-gdown -O $(pwd)/coastlines.zip "https://drive.google.com/uc?export=download&id=1fx_WeWbRmSn9gWC8fuWAIUksaIQLay6u"
-unzip $(pwd)/coastlines.zip
-rm $(pwd)/coastlines.zip
+gdown -O change_detection/ratio_method/download_data/coastlines.zip "https://drive.google.com/uc?export=download&id=1fx_WeWbRmSn9gWC8fuWAIUksaIQLay6u"
+unzip change_detection/ratio_method/download_data/coastlines.zip
+rm change_detection/ratio_method/download_data/coastlines.zip
